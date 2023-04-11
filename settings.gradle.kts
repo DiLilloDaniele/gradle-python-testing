6fcd3b9ff650e2e865cb827b026c4c86ef93faf9
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-04-11 17:07:39 +0200

    chore(build): added pre-commit hook and git msg check

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,23 @@
+plugins {
+    id("com.gradle.enterprise") version "3.11.1"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.6"
+}
+
 enableFeaturePreview("VERSION_CATALOGS")
 rootProject.name = "gradle-python-testing"
 
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks(true)
+}
